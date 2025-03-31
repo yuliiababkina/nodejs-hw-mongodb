29a4103f36d0264ce6fffea2ca6dda82a84223fe
Yuliia Babkina <yulichka.babkina1312@gmail.com>
2025-03-30 22:28:02 -0300

    add swagger documentation

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import router from './routers/index.js';
+
+import { UPLOAD_DIR } from './constants/index.js';
+
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
+
 import { getEnvVar } from './utils/getEnvVar.js';
-import cookieParser from 'cookie-parser';
-import { UPLOAD_DIR } from './constants/index.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -39,9 +43,10 @@ export const setupServer = () => {
 
   app.use(router);
 
-  app.use('*', notFoundHandler);
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
+  app.use('*', notFoundHandler);
   app.use(errorHandler);
 
   app.listen(PORT, () => {
